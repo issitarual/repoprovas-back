d31a34d6822364eccbc512d202caed407b7f9eef
Laura <laura.tracierra@gmail.com>
2021-07-29 09:06:38 -0300

    comentando parte de typeorm e banco teste

--- src/database.ts
+++ src/database.ts
@@ -1,6 +1,6 @@
 import { getConnectionManager } from "typeorm";
 
-export default async function connect () {
+/* export default async function connect () {
     const connectionManager = await getConnectionManager();
     const connection = connectionManager.create({
       name: "default",
@@ -10,4 +10,19 @@ export default async function connect () {
     });
     await connection.connect();
     return connection;
-  }
+  } */
+
+import pg from 'pg';
+
+const { Pool } = pg;
+
+const databaseConfig = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+}
+
+const connection = new Pool(databaseConfig);
+
+export default connection;
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
 import './setup';
 import app, { init } from "./app";
 
-init().then(() => {
+/* init().then(() => {
   app.listen(process.env.PORT, () => {
     console.log('Server is listening on port' + process.env.PORT);
   });
+}); */
+
+app.listen(process.env.PORT, () => {
+  console.log("Server running on port " + process.env.PORT);
 });
