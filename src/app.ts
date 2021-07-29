cf548952b753a7737568f7f87d43ef167b6f7f18
Laura <laura.tracierra@gmail.com>
2021-07-29 19:18:18 -0300

    corrigindo rota de submit

--- src/app.ts
+++ src/app.ts
@@ -22,7 +22,7 @@ app.get("/", (req, res) => {
 app.get("/submit", subject);
 app.get("/submit/subject/:id", professorBySubject);
 app.get("/submit/types", type);
-app.post("submit", newTest)
+app.post("/submit", newTest)
 
 export async function init () {
   await connectDatabase();
