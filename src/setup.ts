3fbe5512179d6f8452afe877a40d6bcf8b7e1190
Laura <laura.tracierra@gmail.com>
2021-07-29 08:48:00 -0300

    modificao setup

--- src/setup.ts
+++ src/setup.ts
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 
-const envFile = process.env.NODE_ENV === 'test' ? '.env.test' : '.env';
+const envFile = process.env.NODE_ENV === 'test' ? '.env.test' : process.env.DB_URL;
 
 dotenv.config({
   path: envFile
