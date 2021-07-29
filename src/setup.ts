c596ec2d7c67ee282a5644405cfc00902b493dbb
Laura <laura.tracierra@gmail.com>
2021-07-29 08:54:50 -0300

    modificando url do banco

--- src/setup.ts
+++ src/setup.ts
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 
-const envFile = process.env.NODE_ENV === 'test' ? '.env.test' : process.env.DB_URL;
+const envFile = process.env.NODE_ENV === 'test' ? '.env.test' : '.env';
 
 dotenv.config({
   path: envFile
