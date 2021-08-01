5f12737fd0214182e0ad0da6dcb942bc77a75d14
Laura <laura.tracierra@gmail.com>
2021-08-01 19:31:02 -0300

    tentativa de melhoria para pegar nome de professores

--- src/repositories/professorsRepositories.ts
+++ src/repositories/professorsRepositories.ts
@@ -3,7 +3,16 @@ import Professor from "../entities/professor";
 
 async function allProfessors(){
     const professor = await getRepository(Professor).find();
-    return professor;
+    let object: any = {};
+    let removeDuplicate = [];
+    for (let i = 0; i < professor.length; i++) {
+        let item = professor[i];
+        if (!object[item.name]) {
+            object[item.name] = true;
+            removeDuplicate.push(item);
+        }
+    }
+    return removeDuplicate;
 }
 
 export { allProfessors };
