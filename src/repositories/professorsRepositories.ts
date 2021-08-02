9f23f000b823d9ae4fc3063c554b8b5fce9841c9
Laura <laura.tracierra@gmail.com>
2021-08-02 15:21:12 -0300

    enviando numero de provas de cada professor

--- src/repositories/professorsRepositories.ts
+++ src/repositories/professorsRepositories.ts
@@ -5,7 +5,9 @@ import Tests from "../entities/test";
 import { idSchema } from "../schemas/idSechemas";
 
 async function allProfessors(){
-    const professor = await getRepository(Professor).find();
+    const professor = await getRepository(Professor).find({
+        relations: ["tests"]
+    });
     let object: any = {};
     let removeDuplicate = [];
     for (let i = 0; i < professor.length; i++) {
